d4e17f03587ec01bd098ba17268194ef7fea3c8a
GossTho <T.Gossenreiter@students.htl-leonding.ac.at>
2024-04-04 11:38:57 +0200

    Image upload added

---
+++ addContentToDB/uploadImage.php
@@ -0,0 +1,64 @@
+<?php
+$target_dir = "../learnhub/img/uploadedImages/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+        $_db_host = "7.tcp.eu.ngrok.io:10359";
+        $_db_database = "learnhub";
+        $_db_username = "learnhub";
+        $_db_password = "learnhub";
+        $conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
+        //Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $insertStatement = "INSERT INTO images (id, path) VALUES ('', '$target_file');";
+        if ($_res = $conn->query($insertStatement)) {
+            echo "<br>Image $target_file has been added to the database.";
+        } else {
+            echo "<br>NO insertion into database";
+        }
+        # close database
+        $conn->close();
+    }
+    else{
+        echo "hahdsoöhfösah";
+    }
+}
+include("uploadImage.html");
