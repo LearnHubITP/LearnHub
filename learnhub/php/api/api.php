dac99cadb353b93b6212732fa57097e0cea7aab7
GossTho <T.Gossenreiter@students.htl-leonding.ac.at>
2024-04-11 15:46:19 +0200

    Added API (Not tested)

--- learnhub/php/api/api.php
+++ learnhub/php/api/api.php
@@ -8,13 +8,108 @@ $answer = array(
     "result" => []
 );
 
+// REQUEST FOR CHAPTERS
+if (isset($_GET["chapter"]) && isset($_GET["subject"]) && isset($_GET["year"])) {
+    $sqlStatement = "SELECT * FROM subjects WHERE name = " . $_GET["subject"];
+    if($res =$conn->query($sqlStatement)){
+        if($res->num_rows > 0){
+            $subject = $res->fetch_assoc();
+            $subjectId = $subject["id"];
+        }
+        else{
+            echo json_encode($answer);
+            exit;
+        }
+    }
+    else{
+        echo json_encode($answer);
+        exit;
+    }
+
+    $sqlStatement = "SELECT * FROM years WHERE name = " . $_GET["year"];
+    if($res =$conn->query($sqlStatement)){
+        if($res->num_rows > 0){
+            $year = $res->fetch_assoc();
+            $yearId = $year["id"];
+        }
+        else{
+            echo json_encode($answer);
+            exit;
+        }
+    }
+    else{
+        echo json_encode($answer);
+        exit;
+    }
+    
+
+    if($_GET["chapter"] == ""){
+        $sqlStatement = "SELECT * FROM chapters WHERE subject = '$subjectId' AND year = '$yearId'";
+    }
+    else{
+        $sqlStatement = "SELECT * FROM chapters WHERE id = " . $_GET["chapter"] . " AND subject = '$subjectId' AND year = '$yearId'";
+    }
+
+    if($res =$conn->query($sqlStatement)){
+        if($res->num_rows > 0){
+
+            $answer["code"] = 200;
+            while($chapter = $res->fetch_assoc()){
+                array_push($answer["result"], array("name"=>$chapter["name"]));
+            }
+
+        }
+    }
+}
+//REQUEST FOR NOTES
+else if (isset($_GET["note"]) && isset($_GET["chapter"])) {
+    
+    $sqlStatement = "SELECT * FROM chapters WHERE id = " . $_GET["chapter"];
+    if($res =$conn->query($sqlStatement)){
+        if($res->num_rows > 0){
+
+            $answer["code"] = 200;
+            $chapter = $res->fetch_assoc();
+            $chapterName = $chapter["name"];
+            $chapterId = $chapter["id"];
+            
+            $sqlStatement = "SELECT * FROM notes WHERE chapter = '$chapterId'";
+            if($res =$conn->query($sqlStatement)){
+                if($res->num_rows > 0){
+
+                    $note = $res->fetch_assoc();
+                    $imageId = $note["image"];
+
+                    $sqlStatement = "SELECT * FROM images WHERE id = '$imageId'";
+                    if($res =$conn->query($sqlStatement)){
+                        if($res->num_rows > 0){
+                            
+                            $image = $res->fetch_assoc();
+                            $imagePath = $image["path"];
+                            array_push(
+                                $answer["result"], 
+                                array(
+                                    "chapter"=>$chapterName,
+                                    "image"=>$imagePath
+                                )
+                            );
+
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
 // REQUEST FOR SUBJECTS
-if (isset($_GET["subjectId"])) {
-    if($_GET["subjectId"] == ""){
+else if (isset($_GET["subject"])) {
+    if($_GET["subject"] == ""){
         $sqlStatement = "SELECT * FROM subjects";
     }
     else{
-        $sqlStatement = "SELECT * FROM subjects WHERE id = " . $_GET["subjectId"];
+        $sqlStatement = "SELECT * FROM subjects WHERE name = " . $_GET["subject"];
     }
 
     if($res =$conn->query($sqlStatement)){
@@ -22,19 +117,19 @@ if (isset($_GET["subjectId"])) {
 
             $answer["code"] = 200;
             while($subject = $res->fetch_assoc()){
-                array_push($answer["result"], $subject["name"]);
+                array_push($answer["result"], array("name"=>$subject["name"]));
             }
 
         }
     }
 }
 // REQUEST FOR YEARS
-else if (isset($_GET["yearId"])) {
-    if($_GET["yearId"] == ""){
+else if (isset($_GET["year"])) {
+    if($_GET["year"] == ""){
         $sqlStatement = "SELECT * FROM years";
     }
     else{
-        $sqlStatement = "SELECT * FROM years WHERE id = " . $_GET["yearId"];
+        $sqlStatement = "SELECT * FROM years WHERE name = " . $_GET["year"];
     }
 
     if($res =$conn->query($sqlStatement)){
@@ -42,119 +137,12 @@ else if (isset($_GET["yearId"])) {
 
             $answer["code"] = 200;
             while($year = $res->fetch_assoc()){
-                array_push($answer["result"], $year["name"]);
+                array_push($answer["result"], array("name"=>$year["name"]));
             }
 
         }
     }
 }
-// REQUEST FOR CHAPTERS
-else if (isset($_GET["chapterId"]) && isset($_GET["subject"]) && isset($_GET["year"])) {
-    $sqlStatement = "SELECT * FROM subjects WHERE name = " . $_GET["subject"];
-    if($res =$conn->query($sqlStatement)){
-        if($res->num_rows > 0){
-            $subject = $res->fetch_assoc();
-            $subjectId = $subject["id"];
-        }
-        else{
-            echo json_encode($answer);
-            exit;
-        }
-    }
-    else{
-        echo json_encode($answer);
-        exit;
-    }
-
-    $sqlStatement = "SELECT * FROM years WHERE name = " . $_GET["year"];
-    if($res =$conn->query($sqlStatement)){
-        if($res->num_rows > 0){
-            $year = $res->fetch_assoc();
-            $yearId = $year["id"];
-        }
-        else{
-            echo json_encode($answer);
-            exit;
-        }
-    }
-    else{
-        echo json_encode($answer);
-        exit;
-    }
-    
-
-    if($_GET["chapterId"] == ""){
-        $sqlStatement = "SELECT * FROM chapters WHERE subject = 'subjectId' AND year = 'yearId'";
-    }
-    else{
-        $sqlStatement = "SELECT * FROM chapters WHERE id = " . $_GET["chapterId"] . " AND subject = 'subjectId' AND year = 'yearId'";
-    }
-
-    if($res =$conn->query($sqlStatement)){
-        if($res->num_rows > 0){
-
-            $answer["code"] = 200;
-            while($chapter = $res->fetch_assoc()){
-                array_push($answer["result"], $chapter["name"]);
-            }
-
-        }
-    }
-}
-// REQUEST FOR NOTES
-else if (isset($_GET["noteId"]) && isset($_GET["subject"]) && isset($_GET["year"])) {
-    $sqlStatement = "SELECT * FROM subjects WHERE name = " . $_GET["subject"];
-    if($res =$conn->query($sqlStatement)){
-        if($res->num_rows > 0){
-            $subject = $res->fetch_assoc();
-            $subjectId = $subject["id"];
-        }
-        else{
-            echo json_encode($answer);
-            exit;
-        }
-    }
-    else{
-        echo json_encode($answer);
-        exit;
-    }
-
-    $sqlStatement = "SELECT * FROM years WHERE name = " . $_GET["year"];
-    if($res =$conn->query($sqlStatement)){
-        if($res->num_rows > 0){
-            $year = $res->fetch_assoc();
-            $yearId = $year["id"];
-        }
-        else{
-            echo json_encode($answer);
-            exit;
-        }
-    }
-    else{
-        echo json_encode($answer);
-        exit;
-    }
-    
-
-    if($_GET["chapterId"] == ""){
-        $sqlStatement = "SELECT * FROM chapters WHERE subject = 'subjectId' AND year = 'yearId'";
-    }
-    else{
-        $sqlStatement = "SELECT * FROM chapters WHERE id = " . $_GET["chapterId"] . " AND subject = 'subjectId' AND year = 'yearId'";
-    }
-
-    if($res =$conn->query($sqlStatement)){
-        if($res->num_rows > 0){
-
-            $answer["code"] = 200;
-            while($chapter = $res->fetch_assoc()){
-                array_push($answer["result"], $chapter["name"]);
-            }
-
-        }
-    }
-}
-
 
 // SEND JSON
 echo json_encode($answer);
