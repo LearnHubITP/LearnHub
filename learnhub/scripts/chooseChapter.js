3d3db83e8c0a669d513079f58ba9cd84e6c0a5d2
Isabella Baumann <i.baumann@students.htl-leonding.ac.at>
2024-04-25 14:24:32 +0200

    show chapters api (mit bootstrap :( )

--- learnhub/scripts/chooseChapter.js
+++ learnhub/scripts/chooseChapter.js
@@ -1,18 +1,21 @@
-chooseYear()
-function chooseYear(){
-    fetch("../php/api/api.php?year=")  
+chooseChapter()
+
+function chooseChapter(){
+    let actSubject = JSON.parse(localStorage['actSubject'])
+    let actYear = JSON.parse(localStorage['actYear'])
+    console.log(actSubject + " " + actYear)
+    fetch(`../php/api/api.php?subject=${actSubject}&year=${actYear}&chapter=`)   // + chaptername, if you want a specific chapter
     .then((response) => response.json())
     .then((data) => {
-        let actSubject = JSON.parse(localStorage['actSubject'])
         console.log(data);
         let html_code = ``
         html_code += `<div id="chooseYear">
                         <div id="alignYear">
                             <div id="yearContainer">
                             <div>
-                            <div><h3 id="headerYearSubj">${actSubject}</h3></div>`
+                            <div><h3 id="headerYearSubj">${actYear}. Jahrgang: ${actSubject}</h3></div>`
             for(let i = 0; i < data.result.length; i++){
-                html_code += `<div onclick="saveYear(${data.result[i].name})" class="year">${data.result[i].name}. Jahrgang</div>`
+                html_code += `<div onclick="saveYear(${data.result[i].name})" class="year">${data.result[i].name}</div>`
             }
                                 
         html_code +=         `</div>
@@ -20,16 +23,8 @@ function chooseYear(){
                         </div>
                     </div>`
         document.getElementById('contentShow').innerHTML = html_code;
-        
     })
     .catch((error) => {
         console.log(error);
     });
-}
-
-// localstorage speichern, aktuelle daten
-
-function saveYear(actYear){
-    localStorage['actYear'] = JSON.stringify(actYear);
-    window.open('../contentOutputHTML/chooseChapter.html', '_self');
 }
