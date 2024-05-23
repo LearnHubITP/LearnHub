d2841c9dd273ea64f3955b05acf72ab061c2ea94
GossTho <T.Gossenreiter@students.htl-leonding.ac.at>
2024-05-23 10:52:33 +0200

    Added note showing functionality

--- learnhub/scripts/chooseChapter.js
+++ learnhub/scripts/chooseChapter.js
@@ -15,7 +15,7 @@ function chooseChapter(){
                             <div>
                             <div><h3 id="headerYearSubj">${actYear}. Jahrgang: ${actSubject}</h3></div>`
             for(let i = 0; i < data.result.length; i++){
-                html_code += `<div onclick="saveYear(${data.result[i].name})" class="year">${data.result[i].name}</div>`
+                html_code += `<div onclick="saveChapter('${data.result[i].name}')" class="year">${data.result[i].name}</div>`
             }
                                 
         html_code +=         `</div>
@@ -29,7 +29,8 @@ function chooseChapter(){
     });
 }
 
-function saveYear(actChapter){
+
+function saveChapter(actChapter){
     localStorage['actChapter'] = JSON.stringify(actChapter);
-    window.open('../contentOutputHTML/chooseYear.html', '_self');
+    window.open('../contentOutputHTML/showNote.html', '_self');
 }
---
+++ learnhub/scripts/showNote.js
@@ -0,0 +1,30 @@
+showNote()
+
+function showNote(){
+    let actChapter = JSON.parse(localStorage['actChapter'])
+    fetch(`../php/api/api.php?chapter=${actChapter}&note=`)   
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        // Erstelle ein <object>-Element für das PDF
+        var object = document.createElement('object');
+        object.data = `.${data.result[0].image}`;
+        object.type = 'application/pdf';
+        object.width = '100%';
+        object.height = '135%';
+        object.style.marginTop = "-7.3%"
+
+        // Füge das <object>-Element dem Container in der Webseite hinzu
+        var container = document.getElementById('contentShow');
+        container.appendChild(object);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
+function saveChapter(actChapter){
+    localStorage['actChapter'] = JSON.stringify(actChapter);
+    window.open('../contentOutputHTML/showNote.html', '_self');
+}
