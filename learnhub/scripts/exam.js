7024200c5260b6e13cce1b620974f9578402e992
GossTho <T.Gossenreiter@students.htl-leonding.ac.at>
2024-06-04 13:44:50 +0200

    Startet working on the exam site

---
+++ learnhub/scripts/exam.js
@@ -0,0 +1,29 @@
+showNote()
+
+function showNote(){
+    let actChapter = JSON.parse(localStorage['actChapter'])
+    fetch(`../php/api/api.php?chapter=${actChapter}&note=`)   
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        // Erstelle ein <object>-Element für das PDF
+        var object = document.createElement('object');
+        object.data = `.${data.result[0].image}`;
+        object.type = 'application/pdf';
+        object.width = '100%';
+        object.height = '135%';
+        object.style.marginTop = "-7.3%"
+
+        // Füge das <object>-Element dem Container in der Webseite hinzu
+        var container = document.getElementById('contentShow');
+        container.appendChild(object);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
+function openExamPopup(){
+    
+}
