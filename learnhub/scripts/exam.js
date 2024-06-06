fde0796470142c01a8bb422c47841e929f11b1ec
GossTho <T.Gossenreiter@students.htl-leonding.ac.at>
2024-06-06 10:55:23 +0200

    Working on questions exam

--- learnhub/scripts/exam.js
+++ learnhub/scripts/exam.js
@@ -1,29 +1,50 @@
-showNote()
+let questions;
+let currentQuestionIndex = 0;
 
-function showNote(){
+let currentQuestion = document.getElementById('currentQuestion');
+let question = document.getElementById("question");
+let questionImg = document.getElementById("questionImg");
+let answerInputContainer = document.getElementById("answerInputContainer");
+let answerContainerChoices = document.getElementById("answerContainerChoices");
+let answerInput = document.getElementById("answerInput");
+
+loadQuestions();
+
+function loadQuestions() {
     let actChapter = JSON.parse(localStorage['actChapter'])
-    fetch(`../php/api/api.php?chapter=${actChapter}&note=`)   
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        // Erstelle ein <object>-Element für das PDF
-        var object = document.createElement('object');
-        object.data = `.${data.result[0].image}`;
-        object.type = 'application/pdf';
-        object.width = '100%';
-        object.height = '135%';
-        object.style.marginTop = "-7.3%"
-
-        // Füge das <object>-Element dem Container in der Webseite hinzu
-        var container = document.getElementById('contentShow');
-        container.appendChild(object);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+    fetch("../php/api/getQuestion.php?chapter=" + actChapter)  
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            questions = data.result;
+            showNextQuestion() 
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 
-function openExamPopup(){
-    
+function showNextQuestion() {
+    let currQuestion = questions[currentQuestionIndex];
+    currentQuestion.innerHTML = "Aufgabe " + (currentQuestionIndex+1);
+    question.innerHTML = currQuestion.question;
+
+    if (currQuestion.img != null){
+        questionImg.src = currQuestion.img;
+        questionImg.style.display = "block";
+    } else {
+        questionImg.src = "";
+        questionImg.style.display = "none";
+    }
+
+    if (currQuestion.choices.length > 0){
+        answerInputContainer.style.display = "none";
+        answerContainerChoices.style.display = "block";
+    } else {
+        answerContainerChoices.style.display = "none";
+        answerInputContainer.style.display = "flex";
+    }
+
+    answerInput.value = "";
 }
